4205111c1c157d5c4a6cf39f48edca8fa5cc8997
MVM <luis.covarrubias.fv@gmail.com>
2025-10-21 20:06:10 -0600

    Change report prefix from 'MC' to 'Service'

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function generateFolio(){
     const now = new Date();
     const y = now.getFullYear(), m = String(now.getMonth()+1).padStart(2,'0'), d = String(now.getDate()).padStart(2,'0');
     const h = String(now.getHours()).padStart(2,'0'), min = String(now.getMinutes()).padStart(2,'0');
-    return `MC_Report-${company}-${y}${m}${d}-${h}${min}`;
+    return `Service_Report-${company}-${y}${m}${d}-${h}${min}`;
 }
 /// ======================
 // GUARDAR REGISTRO (¡CORRECCIÓN FINAL!)
